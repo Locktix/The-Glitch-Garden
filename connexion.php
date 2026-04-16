4e24d9ba17fba21624ad12cb86d5a6e5869df3e7
Locktix <alanplokain@hotmail.com>
2026-04-16 12:14:33 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,16 +1,33 @@
 <?php
     $page = 'connexion';
     include 'app/view/header.php';
+
+    $email = '';
+    $password = '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $email = htmlspecialchars($_POST['email'] ?? '');
+        $password = htmlspecialchars($_POST['password'] ?? '');
+
+        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailIsValide = true;
+        }
+
+        if (empty($password)) {
+            $passwordIsValide = true;
+        }
+    }
+
 ?>
         <section class="login-section">
             <h1>Connexion</h1>
             <form action="#" method="post" class="login-form">
                 <label for="email">Adresse e-mail :</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" value="<?= $email ?>" required>
 
                 <label for="password">Mot de passe :</label>
                 <div class="password-container">
-                    <input type="password" id="password" name="password" required>
+                    <input type="password" id="password" name="password" value="<?= $password ?>" required>
                 </div>
                 <button type="submit">Se connecter</button>
                 <div class="form-links">
